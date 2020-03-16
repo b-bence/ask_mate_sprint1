d0a41cfb2ce48441c5e6ee9f31bd92a3de0b252a
b-bence <borsi92@gmail.com>
2020-03-16 22:32:12 +0100

    display question, add question added

--- data_handler.py
+++ data_handler.py
@@ -1,6 +1,6 @@
 import csv
-question_headers = ['id','submission_time','view_number','vote_number','title','message', 'image']
-answer_headers = ['id','submission_time','vote_number', 'question_id','message','image']
+csv_question_headers = ['id','submission_time','view_number','vote_number','title','message', 'image']
+csv_answer_headers = ['id','submission_time','vote_number', 'question_id','message','image']
 
 question_table_headers = ['ID', 'Submission time', 'View number', 'Vote number', 'Title', 'Message', 'Image']
 
@@ -10,4 +10,28 @@ def get_questions():
         csv_reader = csv.DictReader(csv_file)
         lst = [x for x in csv_reader]
     return lst
+
+
+def generate_question_id():
+    max_id = 0
+    with open('questions.csv', 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for i in csv_reader:
+            if int(i['id']) >= max_id:
+                max_id = int(i['id']) + 1
+    return max_id
+
+
+def write_question_data(lst):
+    with open('questions.csv', 'a') as file:
+        wr = csv.writer(file)
+        wr.writerow(lst)
+
+
+def get_answers():
+    with open('answers.csv', 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        lst = [x for x in csv_reader]
+    return lst
+
 
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import data_handler
 
 app = Flask(__name__)
@@ -14,6 +14,42 @@ def list():
     questions = data_handler.get_questions()
     table_headers = data_handler.question_table_headers
     return render_template('list.html', table_headers=table_headers, questions=questions)
+
+
+@app.route("/add-question", methods=['GET', 'POST'])
+def add_question():
+    if request.method == 'POST':
+        id = data_handler.generate_question_id()
+        submission = 'None'
+        view = 0
+        vote = 0
+        title = request.form['title']
+        message = request.form['message']
+        data = [id, submission, view, vote, title, message]
+
+        data_handler.write_question_data(data)
+
+        return redirect(f'/question/{id}')
+    return render_template('add-question.html')
+
+
+@app.route('/question/<question_id>', methods=['GET','POST'])
+def id(question_id):
+    row_num = 0
+    table_headers = data_handler.question_table_headers
+    questions = data_handler.get_questions()
+    answers=data_handler.get_answers()
+    for index, row in enumerate(questions):
+        if row['id'] == question_id:
+            row_num = index
+    question_data = questions[row_num]
+    try:
+        answer_data = answers[row_num]
+    except IndexError:
+        answer_data = "No answer yet"
+
+    return render_template('display_question.html'
+                           , question_data=question_data, answer_data=answer_data, table_headers=table_headers)
 
 
 if __name__ == "__main__":
