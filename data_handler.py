ef3e11c3a959f9c2598548d9c7675a8c3973405e
b-bence <borsi92@gmail.com>
2020-03-16 14:57:06 +0100

    list q ready

---
+++ data_handler.py
@@ -0,0 +1,4 @@
+question_headers = ['id','submission_time','view_number','vote_number','title','message']
+answer_headers = ['id','submission_time','vote_number', 'question_id','message','image']
+
+question_table_headers = ['ID', 'Submission time', 'View number', 'Vote number', 'Title', 'Message']
--- server.py
+++ server.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+import data_handler
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def main_page():
+    return render_template('main_page.html')
+
+
+@app.route("/list")
+def list():
+    table_headers = data_handler.question_table_headers
+    return render_template('list.html', table_headers=table_headers)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(
+        debug='true'
+    )
